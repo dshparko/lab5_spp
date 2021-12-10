a2c4d682908e298aec7c9ae258ded8dc2736ff6e
Lenovo <d.shparko@mail.ru>
2021-12-10 22:04:17 +0300

    add other classes

---
+++ DependencyInjectionContainerLib/DependenciesConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DependenciesConfiguration
+    {
+        internal Dictionary<Type, List<Dependency>> Dependencies { get; }
+
+        public DependenciesConfiguration()
+        {
+            Dependencies = new Dictionary<Type, List<Dependency>>();
+        }
+
+
+        public void Register<TInterface, TImplementation>(LifeCycle lifeCycle)
+            where TInterface : class
+            where TImplementation : class, TInterface
+        {
+            Register(typeof(TInterface), typeof(TImplementation), lifeCycle);
+        }
+
+        public void Register<TInterface, TImplementation>()
+            where TInterface : class
+            where TImplementation : class, TInterface
+        {
+            Register(typeof(TInterface), typeof(TImplementation), LifeCycle.Instance);
+        }
+
+        public void Register(Type @interface, Type type)
+        {
+            Register(@interface, type, LifeCycle.Instance);
+        }
+
+        public void Register(Type @interface, Type type, LifeCycle lifeCycle)
+        {
+            if (Dependencies.ContainsKey(@interface))
+            {
+                if (@interface != type)
+                    Dependencies[@interface].Add(new Dependency(type, lifeCycle));
+            }
+            else
+                Dependencies.Add(@interface, new List<Dependency> {new Dependency(type, lifeCycle)});
+        }
+    }
+}
---
+++ DependencyInjectionContainerLib/DependencyProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DependencyProvider
+    {
+        private DependenciesConfiguration Configuration { get; }
+
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            try
+            {
+                ValidateConfiguration(configuration);
+                Configuration = configuration;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+        }
+        
+        public TInterface Resolve<TInterface>()
+            where TInterface : class
+        {
+            return (TInterface) Resolve(typeof(TInterface));
+        }
+
+        private static void ValidateConfiguration(DependenciesConfiguration configuration)
+        {
+            foreach (var implementations in configuration.Dependencies
+                .SelectMany(
+                    keyValuePair => keyValuePair.Value.Where(implementations => implementations.Type.IsAbstract)))
+            {
+                throw new Exception(implementations + " is abstract class");
+            }
+        }
+
+        private object Resolve(Type interfaceType)
+        {
+            if (interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+            {
+                var @interface = interfaceType.GetGenericArguments()[0];
+                return ClassCreator.CreateClassIEnumerable(@interface, Configuration.Dependencies[@interface],
+                    Configuration);
+            }
+
+            return ClassCreator.CreateClass(Configuration.Dependencies[interfaceType][0], Configuration);
+        }
+    }
+}
